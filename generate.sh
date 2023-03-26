34f1b3c9ca81dad3c6da3f6ed98c6137496901d9
devmegan <devmegan@protonmail.com>
2023-03-26 08:31:16 +0000

    add: bash script error handling if arg type incorrect

--- generate.sh
+++ generate.sh
@@ -8,6 +8,17 @@ if [[ $# -lt 2 ]]; then
     exit 1
 fi
 
+if [[ ! $1 =~ ^[0-9]+$ || ! $2 =~ ^(low|medium|high)$ ]]; then
+    echo "./generate.sh requires arguments in the following format arguments"
+    echo
+    echo "Usage: $0 <length:int> <characters:str[low|medium|high]>"
+    echo
+    echo "    length      : positive integer for length of password."
+    echo "    complexity  : 'low', 'medium', or 'high' for the complexity of the password."
+
+    exit 1
+fi
+
 php -r '
     require_once "oop-generate.php";
     $password = new PasswordGenerator('$1', "'$2'");
