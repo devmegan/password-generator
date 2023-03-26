61759581e69ec7e40e405ef3606d674ed0361f07
devmegan <devmegan@protonmail.com>
2023-03-26 08:30:54 +0000

    add: bash script error handling if not 2 args

--- generate.sh
+++ generate.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ $# -lt 2 ]]; then
+    echo "./generate.sh requires at least 2 arguments"
+    echo
+    echo "Usage: $0 <length:int> <characters:str[low|medium|high]>"
+
+    exit 1
+fi
+
 php -r '
     require_once "oop-generate.php";
     $password = new PasswordGenerator('$1', "'$2'");
